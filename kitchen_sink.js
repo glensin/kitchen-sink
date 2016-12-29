a2609714fdb75862ac183b9c8b4860fde15b5007
Sin Chin Guan <glensin@Sin-Chin-Guan-Glens-MacBook-Air.local>
2016-12-29 14:27:38 +0800

    update with zhaoloong answer

--- kitchen_sink.js
+++ kitchen_sink.js
@@ -60,3 +60,92 @@ else {
 }
 
 arrOb()
+
+function kitchenSink(dataType){
+  switch(typeof dataType){
+    case 'string':
+      console.log(dataType);
+      break;
+    case 'number':
+      console.log(dataType*dataType);
+      break;
+    case 'boolean':
+      dataType ? console.log('yes') : console.log('no');
+      break;
+    case 'function':
+      dataType();
+      break;
+    case 'undefined':
+      console.log(`yo @$%@$%^ what's up with the horrbile input?`);
+      break;
+    case 'object':
+      if (Array.isArray(dataType)) {
+        dataType.map(function(value){
+          kitchenSink(value);
+        })
+      } else if (dataType === null){
+          console.log(`output is null`);
+      } else {
+        Object.keys(dataType).map(function(value2){
+          kitchenSink(dataType[value2]);
+        })
+      }
+  }
+}
+
+function helloWorld(){
+  console.log('hello world!');
+}
+
+kitchenSink('hello');
+kitchenSink(9);
+kitchenSink(true);
+kitchenSink(false);
+kitchenSink(helloWorld);
+kitchenSink(undefined);
+kitchenSink([1,2,3,5,10]);
+kitchenSink({john:'male',sarah:'female',princessLeia:'dead'});
+kitchenSink(null);
+
+function kitchenSinkReturn(dataType){
+  switch(typeof dataType){
+    case 'string':
+      return dataType;
+    case 'number':
+      return dataType*dataType;
+    case 'boolean':
+      return dataType ? 'yes' : 'no';
+    case 'function':
+      return dataType();
+    case 'undefined':
+      return `yo @$%@$%^ what's up with the horrbile input?`
+    case 'object':
+      if (Array.isArray(dataType)) {
+        dataType.map(function(value){
+          return kitchenSink(value);
+        })
+      } else if (dataType === null){
+          return `output is null`;
+      } else {
+        Object.keys(dataType).map(function(value2){
+          return kitchenSink(dataType[value2]);
+        })
+      }
+  }
+}
+
+function helloWorld2(){
+  return 'hello world!';
+}
+
+console.log('<----------------------------------------------------------------------->')
+
+console.log(kitchenSinkReturn('hello'));
+console.log(kitchenSinkReturn(9));
+console.log(kitchenSinkReturn(true));
+console.log(kitchenSinkReturn(false));
+console.log(kitchenSinkReturn(helloWorld2));
+console.log(kitchenSinkReturn(undefined));
+console.log(kitchenSinkReturn([1,2,3,5,10]));
+console.log(kitchenSinkReturn({john:'male',sarah:'female',princessLeia:'dead'}));
+console.log(kitchenSinkReturn(null));
